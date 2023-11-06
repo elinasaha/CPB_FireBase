5107bca44d6f55a8d527a9b294a85f74e6eff853
elinasaha <esaha6359@stu.d214.org>
2023-11-06 10:27:42 -0600

    Initial Commit

---
+++ CPB_FireBase/CPB_FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPB_FireBaseApp.swift
+//  CPB_FireBase
+//
+//  Created by Elina Saha on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CPB_FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPB_FireBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPB_FireBase
+//
+//  Created by Elina Saha on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
